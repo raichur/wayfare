01a7827855d7bf4e58a0ddecf1dedec98e6412d3
Joshua Raichur <raichur@utexas.edu>
2019-07-27 18:12:55 -0500

    Change nav based on auth status

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,14 +4,15 @@ import LoginLinks from './LoginLinks';
 import LogoutLinks from './LogoutLinks';
 import { connect } from 'react-redux';
 
-const Nav = () => {
+const Nav = (props) => {
+    const { auth } = props;
+    const links = auth.uid ? <LoginLinks/> : <LogoutLinks/>
     return (
         <nav>
             <ul>
                 <Link to='/' className="title">Netcome</Link>
                 <div className="right">
-                    <LogoutLinks />
-                    <LoginLinks />
+                    {links}
                 </div>
             </ul>
         </nav>
@@ -19,9 +20,8 @@ const Nav = () => {
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return {
-
+        auth: state.firebase.auth
     }
 }
 
