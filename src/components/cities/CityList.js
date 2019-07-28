a35d9e82b530e1677ebd6bf77e7fa8417461d91a
Joshua Raichur <raichur@utexas.edu>
2019-07-28 12:38:56 -0500

    City select dropdown

--- src/components/cities/CityList.js
+++ src/components/cities/CityList.js
@@ -2,26 +2,43 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { changeCurrentCity } from '../../store/actions/cityActions';
+import Select from 'react-select'
 
 class CityList extends Component {
 
-    changeCurrentCity = (cityid, userId) => {
-        this.props.changeCurrentCity(cityid, userId);
+    changeCurrentCity = (e) => {
+        this.props.changeCurrentCity(e.value, e.user);
     }
-
+    
     render() {
+        let cityList = [];
+        if (this.props.cities) {
+            this.props.cities.map(city => {
+                return cityList.push({ value: city.id, label: city.name, user: city.userid})
+            })
+        }
         return (
             <ul className="cities">
-                {this.props.cities ? this.props.cities.map(city => {
+                { this.props.cities ?
+                <Select 
+                options={cityList}
+                isSearchable={true}
+                onChange={this.changeCurrentCity}
+                defaultValue={cityList.filter(option => option.value === this.props.currentcity)}
+                />
+                : null}
+                {/* {this.props.cities ? this.props.cities.map(city => {
                     return (
                     <li key={city.id} className={this.props.currentcity === city.id ? "currentcity" : null}>
                         <Link to="/" onClick={() => this.changeCurrentCity(city.id, city.userid)}>
-                            <h2>{city.name}</h2>
+                            {city.name}
                         </Link>
                     </li>
                     )
-                }) : null}
-                <Link to='/'>+ Add City</Link>
+                }) : null} */}
+                {/* <li>
+                    <Link to='/'>+ Add City</Link>
+                </li> */}
             </ul>
         )
     }
